7bafc36ad67db9ef8e1f51de7f68c9891f681a34
Omar Ramos <omargrr21@gmail.com>
2022-07-21 14:05:34 -0600

    feats: added datetime and other HTTP verbs

--- app/main.py
+++ app/main.py
@@ -12,19 +12,17 @@ CORS(app)
 
 post_report_args= reqparse.RequestParser()
 
-post_report_args.add_argument("id", type=int, help="ERROR id value needs to be an integer", required=True)
 post_report_args.add_argument("classroom", type=str, help="ERROR classroom is required", required=True)
-post_report_args.add_argument("reporter", type=str, help="ERROR reporter is required", required=True)
-post_report_args.add_argument("date", type=str, required=False)
+post_report_args.add_argument("reporter_badge", type=int, help="ERROR reporter_badge is required", required=True)
 post_report_args.add_argument("tag", type=str, help="ERROR tag is required", required=True)
 post_report_args.add_argument("reason", type=str, help="ERROR reason is required", required=True)
 
-@app.route('/test', methods=['GET'])
+@app.route('/test/', methods=['GET'])
 def get_test():
     return jsonify({"message":"You are connected"})
 
 #Classrooms
-@app.route('/classrooms', methods=['GET'])
+@app.route('/classrooms/', methods=['GET'])
 def get_classrooms():
     collection_name= database.db['classrooms']
 
@@ -35,9 +33,9 @@ def get_classrooms():
         classrooms.append(classroom)
     return jsonify(classrooms)
 
-@app.route('/classrooms/<room>', methods=['GET'])
+@app.route('/classrooms/<room>/', methods=['GET'])
 def get_classroom(room):
-    collection_name= database.db['classroom2']
+    collection_name= database.db['classrooms']
 
     request= collection_name.find({'classrooms.id_classroom' : room}, {'classrooms.$' : 1})
     classrooms= []
@@ -45,7 +43,54 @@ def get_classroom(room):
         del classroom['_id']
         return jsonify(classroom)
 
-@app.route('/users', methods=['GET'])
+#Reports
+@app.route('/reports/<room>/', methods=['GET'])
+def get_reports(room):
+    collection_name= database.db['reports']
+    request= collection_name.find({'classroom': room, 'is_active': True})
+    reports= []
+    for report in request:
+        del report['_id']
+        reports.append(report)
+    return jsonify(reports)
+
+
+@app.route('/reports/', methods=['POST'])
+def post_report():
+    collection_name= database.db['reports']
+    date= datetime.now()
+    args = post_report_args.parse_args()
+    report_id= str(date.year)+'-'+str(date.month)+'-'+str(date.day)+'-'+str(args['reporter_badge'])
+    collection_name.insert_one({
+        'report_id' : report_id,
+        'classroom': args['classroom'],
+        'reporter_badge': args['reporter_badge'],
+        'date': date,
+        'tag': args['tag'],
+        'reason': args['reason'],
+        'is_active': True
+    })
+    return jsonify(args)
+
+@app.route('/deactivate/<report_id>/', methods=['PATCH'])
+def patch_report(report_id):
+    collection_name= database.db['reports']
+    collection_name.update_one({'report_id' : report_id}, {'$set': {'is_active' : False}})
+    return jsonify({'Message' : 'Deactivaded'})
+
+#Tags
+@app.route('/tags/', methods=['GET'])
+def get_tags():
+    collection_name= database.db['tags']
+    request= collection_name.find()
+    tags= []
+    for tag in request:
+        del tag['_id']
+        tags.append(tag)
+    return jsonify(tags)
+
+#Users
+@app.route('/users/', methods=['GET'])
 def get_users():
     collection_name= database.db['users']
 
@@ -56,23 +101,10 @@ def get_users():
         users.append(user)
     return jsonify(users)
 
-
-#Tags
-@app.route('/tags/', methods=['GET'])
-def get_tags():
-    collection_name= database.db['tags']
-    request= collection_name.find()
-    tags= []
-    for tag in tags:
-        del tag['_id']
-        tags.append(tag)
-    return jsonify({'tags' : tags})
-
-#Users
-@app.route('/user/<email>', methods=['GET'])
-def get_user(email):
+@app.route('/users/<badge>/', methods=['GET'])
+def get_user(badge):
     collection_name= database.db['users']
-    request= collection_name.find({'email' : email})
+    request= collection_name.find_one({'badge' : badge})
     del request['_id']
     return jsonify(request)
 
